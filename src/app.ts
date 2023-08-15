20b7df99e169b07875dcad0408edbd10056cb84e
hasanulhasan <hasanlink71@yahoo.com>
2023-08-15 21:49:14 +0600

    category create done

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
 import express, { Application } from 'express'
 import cors from 'cors'
 import { UserRoutes } from './modules/user.routes';
+import { CategoryRoutes } from './modules/category/category.routes';
 const app:Application = express();
 app.use(express.json())
 app.use(cors())
 app.use(express.urlencoded({extended: true}))
 
 app.use('/api/v1/user', UserRoutes)
+app.use('/api/v1/category', CategoryRoutes)
 
 export default app;
---
+++ src/modules/category/category.service.ts
@@ -0,0 +1,11 @@
+import { Category, PrismaClient } from "@prisma/client"
+const prisma = new PrismaClient();
+
+const insertToDB = async(data: Category): Promise<Category> => {
+  const result = await prisma.category.create({data})
+  return result
+}
+
+export const CategoryService = {
+  insertToDB
+}
